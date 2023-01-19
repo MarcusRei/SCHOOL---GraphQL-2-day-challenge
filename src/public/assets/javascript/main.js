31661752bdc97287e42469651d416f27d01a4384
MarcusRei <marcusreineck@gmail.com>
2023-01-19 10:06:33 +0100

    Backend working

--- src/public/assets/javascript/main.js
+++ src/public/assets/javascript/main.js
@@ -6,4 +6,11 @@ getAllMoviesBtn.addEventListener("click", () => {
 
 function getAllMovies() {
   console.log("Hello World!");
+
+  const endpoint = process.env.MOVIE_DB_URI;
+
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  console.log(response);
 }
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -17,42 +17,70 @@ const axios = require("axios").default;
 exports.resolvers = {
   Query: {
     getAllMovies: async (_, args) => {
-      let movies = [];
+      let allMovies = [];
       const endpoint = process.env.MOVIE_DB_URI;
       try {
         const response = await axios.get(endpoint);
         if (response.data?.length > 0) {
-          movies = response.data;
+          allMovies = response.data;
         }
       } catch (error) {
         return new GraphQLError("Det finns ju inga filmer här ju!!!");
       }
-      return movies;
+      return allMovies;
     },
   },
   Mutation: {
     createMovie: async (_, args) => {
+      //Skapa input-variabler
+      const { title, description, genre, image } = args.input;
+
       //Verify name, if string is empty, return error
       if ((args, title.length === 0))
         return new GraphQLError("Title must be at least 1 character long");
-
-      //Create filePath??
-
-      //Check if id exists
-      let idAlreadyExists = true;
-      if (!idAlreadyExists)
-        return new GraphQLError("Den här filmen finns ju redan!");
 
       //Create unique id + data object
       const newMovie = {
         id: crypto.randomUUID(),
         title,
         description: description || "",
-        genre: movies.ACTION,
+        genre: movieGenre.ACTION,
         image: image || "",
       };
 
-      try {
+      //Check if id exists
+      const endpoint = process.env.MOVIE_DB_URI;
+      console.log(endpoint);
+
+      const response = await axios.get(endpoint + `/search?id=${newMovie.id}`);
+
+      allMovies = response.data;
+
+      console.log(allMovies);
+
+      if (allMovies.length === 0) {
+        try {
+          const response = await axios.post(
+            endpoint,
+            {
+              data: [newMovie],
+            },
+            {
+              headers: {
+                "Accept-Encoding": "gzip,deflate,compress",
+              },
+            }
+          );
+        } catch (error) {
+          console.error(error);
+          return new GraphQLError("Det gick inte att skapa filmen...");
+        }
+      }
+      /* let idAlreadyExists = true;
+      if (!idAlreadyExists)
+        return new GraphQLError("Den här filmen finns ju redan!"); */
+
+      /* try {
         const endpoint = process.env.MOVIE_DB_URI;
         const response = await axios.post(
           endpoint,
@@ -68,7 +96,7 @@ exports.resolvers = {
       } catch (error) {
         console.error(error);
         return new GraphQLError("Det gick inte att skapa filmen...");
-      }
+      } */
 
       return newMovie;
     },
